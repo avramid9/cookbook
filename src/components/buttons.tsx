e86a5ab3f86a8218b2f3a88c51a01d9873c6971f
avramid9 <andreas.avramidis@alumni.utoronto.ca>
2025-03-12 14:41:18 -0400

    Trying to set prefetch to false on dashboard button links to see if it fixes functionality issues in vercel

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -5,6 +5,7 @@ export function ViewRecipe({ id }: { id: string }) {
     <Link
       href={`/dashboard/${id}`}
       className="btn btn-primary"
+      prefetch = {false}
     >
       Read recipe
     </Link>
